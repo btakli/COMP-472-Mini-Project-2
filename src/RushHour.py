a10caf27cd14712c5bb25319a9b63a8dd575adee
btakli <64867481+btakli@users.noreply.github.com>
2022-11-19 12:18:26 -0500

    feat: :sparkles: Added subset generation for different move lengths

--- src/RushHour.py
+++ src/RushHour.py
@@ -19,21 +19,7 @@ def main():
     board_reader.print_board(0)
     board_reader.print_cars_dict(0)
 
-    # tn = TreeNode(boardslist[0], carslist[0], None, board_reader.exit, 1, 2.0)
-    # print(tn.h_n)
-    # sg = SubStateGenerator(boardslist[0], carslist[0], board_reader.exit)
-    # sg.generate_substates()
-
-    # sg2 = SubStateGenerator(sg.substates[0][0], sg.substates[0][1], board_reader.exit)
-    # sg2.generate_substates()
-    # sg2.print_substates()
-    # for substate in sg2.substates:
-    #     print(TreeNode(substate[0], substate[1], None, board_reader.exit, 1, 2.0).h_n)
-
-    
-    # exit(0)
-
-    search = UniformCostSearch(boardslist[0], carslist[0], board_reader.exit)
+    search = UniformCostSearch(boardslist[5], carslist[5], board_reader.exit)
     win_node = search.search()
 
     print(win_node)
@@ -42,30 +28,30 @@ def main():
     print(search.search_path_length)
     print(search.solution_path)
 
-    #search2 = GBFS(boardslist[0], carslist[0], board_reader.exit, 1, 1)
-    #win_node2 = search2.search()
+    # sg = SubStateGenerator(boardslist[0], carslist[0], board_reader.exit)
+    # sg.generate_substates()
 
-    #print(win_node2)
-    #print(search2.goal.cost)
-    #print(search2.get_exec_time())
-    #print(search2.search_path_length)
-    #print(search2.solution_path)
+    # for substate in sg.substates:
+    #     for row in substate[0]:
+    #         print(row)
+    #     print()
+
+    # t1 = TreeNode(boardslist[0], carslist[0], None, board_reader.exit, 0)
+    # t2 = TreeNode(sg.substates[4][0], sg.substates[4][1], t1, board_reader.exit, 0)
+
+    # print(f"{t2.car_moved} {t2.direction_moved} {t2.distance_moved}")
+    
+
+
+    # search2 = GBFS(boardslist[0], carslist[0], board_reader.exit, 1, 1)
+    # win_node2 = search2.search()
+
+    # print(win_node2)
+    # print(search2.goal.cost)
+    # print(search2.get_exec_time())
+    # print(search2.search_path_length)
+    # print(search2.solution_path)
 
 # main
 if __name__ == "__main__":
     main()
-
-
-# board_manipulator = BoardManipulator(boardslist[0], carslist[0], board_reader.exit)
-# board_manipulator.move_car("M", "down")
-# board_manipulator.move_car("A", "right")
-# board_manipulator.move_car("B", "right")
-# board_manipulator.move_car("C", "right")
-
-# board_manipulator.remove_from_valet_if_exists()
-
-# board_manipulator.print_board()
-# board_manipulator.print_cars_dict()
-
-# board_reader.print_board(0)
-# board_reader.print_cars_dict(0)
