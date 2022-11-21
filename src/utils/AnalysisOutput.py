1d597b398f15b186e982f9628042cc9b0628345d
btakli <64867481+btakli@users.noreply.github.com>
2022-11-20 19:35:18 -0500

    fix: :bug: Fixed newline issue for csv generation on Windows

--- src/utils/AnalysisOutput.py
+++ src/utils/AnalysisOutput.py
@@ -11,12 +11,12 @@ class AnalysisOutput:
         self.file_name = file_name
         self.filepath = os.path.join(self.analysis_folder, self.file_name)
 
-        with open(self.filepath, 'w') as f:
+        with open(self.filepath, 'w', newline="") as f:
             writer = csv.writer(f)
             writer.writerow(['Puzzle Number', 'Algorithm', 'Heuristic', 'Length of the Solution', 'Length of the Search Path', 'Execution Time (in seconds)'])
     
     def add_row(self, puzzle_number: int, algorithm: str, heuristic: str, solution_length: str, search_path_length: int, execution_time: float):
         """Add a row to the CSV file"""
-        with open(self.filepath, 'a') as f:
+        with open(self.filepath, 'a', newline="") as f:
             writer = csv.writer(f)
             writer.writerow([puzzle_number, algorithm, heuristic, solution_length, search_path_length, execution_time])
